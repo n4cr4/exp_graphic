0a6eb2eb3fd420276cbc6c16df7d909ea9bd3fe8
n4cr4 <itosukenao@gmail.com>
2023-06-25 03:44:19 +0900

    Change image target

--- graphic2/create_differentqality.py
+++ graphic2/create_differentqality.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('./img/target.JPG')
+img = cv.imread('./img/q100_s.jpg')
 
 for i in [1,5,10,20,40,80]:
-    cv.imwrite(f'./img/output/target_{i}.jpg', img, [cv.IMWRITE_JPEG_QUALITY, i])
+    cv.imwrite(f'./img/output/q100s_s_{i}.jpg', img, [cv.IMWRITE_JPEG_QUALITY, i])
